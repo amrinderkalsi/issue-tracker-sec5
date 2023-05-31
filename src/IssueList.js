c9dae7c887b8e5bc8a106809eb138c3c09fe74b3
Ammy Kalsi <amrinder.kalsi13.com>
2023-05-31 18:46:04 -0400

    fetched the issues data from server on componentDidMount

--- src/IssueList.js
+++ src/IssueList.js
@@ -2,28 +2,6 @@ import { Component } from 'react';
 import IssueFilter from './IssueFilter';
 import IssueTable from './IssueTable';
 import IssueAdd from './IssueAdd';
-
-const issues = [
-    {
-      id: 1, 
-      status: 'Open', 
-      owner: 'Ravan',
-      created: new Date('2016-08-15'), 
-      effort: 5, 
-      completionDate: undefined,
-      title: 'Error in console when clicking Add',
-    },
-    {
-      id: 2, 
-      status: 'Assigned', 
-      owner: 'Eddie',
-      created: new Date('2016-08-16'), 
-      effort: 14, 
-      completionDate: new Date('2016-08-30'),
-      title: 'Missing bottom border on panel',
-    },
-  ];
-
 class IssueList extends Component {
   constructor() {
     super();
@@ -34,12 +12,24 @@ class IssueList extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ issues: issues });
-    }, 500)
+    fetch('api/issues')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        data.records.forEach(issue => {
+          issue.created = new Date(issue.created);
+          if (issue.completionDate) {
+            issue.completionDate = new Date(issue.completionDate);
+          }
+        });
+        this.setState({ issues: data.records });
+      });
   }
 
   createIssue = (issue) => {
+
+    
+
     issue.id = this.state.issues.length + 1;
     const newIssueArray = [...this.state.issues, issue];
     this.setState({ issues: newIssueArray});
