74002aeed18df1f149d2263b6853d5da5a60f2b6
Ammy Kalsi <amrinder.kalsi13.com>
2023-06-14 18:25:18 -0400

    Added graphql POST request for creating the issue

--- src/IssueList.js
+++ src/IssueList.js
@@ -45,27 +45,59 @@ class IssueList extends Component {
   }
 
   createIssue = (issue) => {
-    fetch('/api/issues', {
+     // GraphQL Mutation for creating the issue
+     fetch('/graphql', { 
       method: 'POST',
       headers: {
         'content-type': 'application/json'
       },
-      body: JSON.stringify(issue)
-    }).then(res => {
-      if (res.ok) {
-        res.json().then(data => {
-          console.log(data)
-          data.created = new Date(data.created);
-          if (data.completionDate) {
-            data.completionDate = new Date(data.completionDate);
+      body: JSON.stringify({
+        query: `mutation Mutation($issue: IssueInputs) {
+          issueAdd(issue: $issue) {
+            completionDate
+            created
+            effort
+            id
+            owner
+            status
+            title
           }
-          const newIssueArray = [...this.state.issues, data];
+        }`, 
+        variables: {issue}
+      })
+    })
+      .then(res => res.json())
+      .then(body => {
+        console.log(body);
+        body.data.issueAdd.created = new Date(body.data.issueAdd.created);
+          if (body.data.issueAdd.completionDate) {
+            body.data.issueAdd.completionDate = new Date(body.data.issueAdd.completionDate);
+          }
+          const newIssueArray = [...this.state.issues, body.data.issueAdd];
           this.setState({ issues: newIssueArray});
-        })
-      } else {
-        res.json().catch(err => {console.log(err)});
-      }
-    }).catch(error => console.log(error));
+      });
+
+    // fetch('/api/issues', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(issue)
+    // }).then(res => {
+    //   if (res.ok) {
+    //     res.json().then(data => {
+    //       console.log(data)
+    //       data.created = new Date(data.created);
+    //       if (data.completionDate) {
+    //         data.completionDate = new Date(data.completionDate);
+    //       }
+    //       const newIssueArray = [...this.state.issues, data];
+    //       this.setState({ issues: newIssueArray});
+    //     })
+    //   } else {
+    //     res.json().catch(err => {console.log(err)});
+    //   }
+    // }).catch(error => console.log(error));
   }
  
     render() { 
